037d70481a0c24f652a7f33670ae5f23669cb20f
Perez <ebukaperez@gmail.com>
2017-12-26 17:18:32 -0500

    Initial Commit

---
+++ BootLegMovie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BootLegMovie
+//
+//  Created by Perez Willie Nwobu on 12/26/17.
+//  Copyright © 2017 Perez Willie Nwobu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
